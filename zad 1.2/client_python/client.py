8fe04828ab47234e52e21d0cf0830698e83b1b90
mlukasie <mateuszlu2106@gmail.com>
2024-11-23 23:46:48 +0100

    Implemented working Alternating Bit Algorithm

--- zad 1.2/client_python/client.py
+++ zad 1.2/client_python/client.py
@@ -5,16 +5,22 @@ HOST = "172.21.36.2"
 UDP_PORT = 5018
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    iteration = 0
-    while True:
-        message = f"{iteration}: Test message".encode()
-        try:
+    s.settimeout(2)
+    message_bit = 0
+    for iteration in range(2000):
+        message = f"{message_bit}{iteration}".encode()
+        while True:
             sent_bytes = s.sendto(message, (HOST, UDP_PORT))
-            print(f"Sent message: {iteration}")
-        except Exception as e:
-            print(f"Couldn't send message: {iteration}")
-            continue
-        iteration = iteration + 1
+            print(f"Sent message: Message bit = {message_bit}, Iteration = {iteration}")
+            try:
+                ack_bit, address = s.recvfrom(256)
+                ack_bit = int(ack_bit.decode())
+                print(f'Received ACK: {ack_bit}')
+                if (ack_bit == message_bit):
+                    message_bit = 1 - message_bit
+                    break
+            except socket.timeout:
+                print("No ACK. Resending.")
         time.sleep(0.2)
 
 s.close()
--- zad 1.2/server_python/server.py
+++ zad 1.2/server_python/server.py
@@ -1,12 +1,27 @@
 import socket
-import time
 
 IP = "172.21.36.2"
 UDP_PORT = 5018
 
 print(f"Server listening on {IP}:{UDP_PORT}")
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    expected_message_bit = 0
     s.bind((IP, UDP_PORT))
     while True:
-        data, addr = s.recvfrom(256)
-        print(f"Received datagram: {data.decode()}")
+        message, addr = s.recvfrom(256)
+        message = message.decode()
+        message_bit = int(message[0])
+        iteration = int(message[1:])
+        print(f'Received message: Message bit = {message_bit} Iteration = {iteration}')
+
+        if(message_bit == expected_message_bit):
+            ack_bit = str(message_bit).encode()
+            s.sendto(ack_bit, addr)
+            print(f'Send ACK: {message_bit}')
+            expected_message_bit = 1 - expected_message_bit
+        
+        else:
+            ack_bit = str(1 - expected_message_bit).encode()
+            s.sendto(ack_bit, addr)
+            print(f'Resending ACK: {1 - expected_message_bit}')
+        
