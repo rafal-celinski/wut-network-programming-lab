257d2e36bdc0ac7d094528bd58667c9763ae0118
mlukasie <mateuszlu2106@gmail.com>
2025-01-15 04:28:00 +0100

    Implemented enryption and decryption, only raport left

--- Projekt/client_python/client.py
+++ Projekt/client_python/client.py
@@ -3,6 +3,10 @@ import socket
 import json
 import threading
 import sys
+from Crypto.Cipher import AES
+from Crypto.Hash import HMAC, SHA256
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import unpad
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--base", type=int)
@@ -18,42 +22,91 @@ BASE = args.base
 MODULUS = args.modulus
 PRIVATE_KEY = args.private_key
 PUBLIC_KEY = (BASE**PRIVATE_KEY) % MODULUS
+SHARED_KEY = -1
 
 RECEIVED_HELLO = False
 HELLO_LOCK = threading.Lock()
 
+def encrypt_message(message):
+    expanded_key = SHA256.new(str(SHARED_KEY).encode('utf-8')).digest()
+    print(SHARED_KEY)
+    
+    
+    iv = get_random_bytes(16)
+    cipher = AES.new(expanded_key, AES.MODE_CBC, iv)
+    
+    padding_length = 16 - (len(message) % 16)
+    padded_message = message + chr(padding_length) * padding_length
+    
+    ciphertext = cipher.encrypt(padded_message.encode('utf-8'))
+    
+    hmac = HMAC.new(expanded_key, digestmod=SHA256)
+    hmac.update(iv + ciphertext)
+
+    print(f"IV: {iv}")
+    print(f"Ciphertext: {ciphertext}")
+    print(f"Computed HMAC: {hmac.digest()}")
+    
+    final_message = iv + ciphertext + hmac.digest()
+    print(final_message)
+    
+    return final_message
+
+
+def decrypt_message(encrypted_message):
+    iv = encrypted_message[:16]  
+    ciphertext = encrypted_message[16:-32]  
+    received_hmac = encrypted_message[-32:]  
+    
+
+    expanded_key = SHA256.new(str(SHARED_KEY).encode('utf-8')).digest()
+    
+
+    hmac = HMAC.new(expanded_key, digestmod=SHA256)
+    hmac.update(iv + ciphertext)
+    try:
+        hmac.verify(received_hmac)
+    except ValueError:
+        raise ValueError("Invalid MAC")
+
+    cipher = AES.new(expanded_key, AES.MODE_CBC, iv)
+    
+
+    decrypted_data = unpad(cipher.decrypt(ciphertext), 16)  
+    return decrypted_data.decode('utf-8')
+
+
 def handle_server_messages(server_socket: socket.socket):
     global RECEIVED_HELLO
+    global SHARED_KEY
     while True:
-        try:
-            response = server_socket.recv(1024)
-            if not response:
-                print("Server has closed the connection. Exiting client.")
-                server_socket.close()
-                sys.exit(0)
+        response = server_socket.recv(1024)
 
-            with HELLO_LOCK:
-                if not RECEIVED_HELLO:
-                    try:
-                        response_message = json.loads(response.decode('utf-8'))
-                        if "type" in response_message and response_message["type"] == "Hello message":
-                            RECEIVED_HELLO = True
-                            print("Received Hello Message")
-                        else:
-                            print("Expected Hello Message, got something else.")
-                    except json.JSONDecodeError:
-                        print("Expected Hello Message, got something else.")
-                else:
-                    response_message = response.decode('utf-8')
-                    if response_message == "EndSession":
-                        print("Session ended by server. To initiate it again send ClientHello")
-                        RECEIVED_HELLO = False
-                    else:
-                        print(f'Received normal message from server: {response_message}')
-        except (ConnectionResetError, ConnectionAbortedError):
-            print("Server has forcibly closed the connection. Exiting client.")
+        if not response:
+            print("Server has closed the connection. Exiting client.")
             server_socket.close()
             sys.exit(0)
+
+        with HELLO_LOCK:
+            if not RECEIVED_HELLO:
+                try:
+                    response_message = json.loads(response.decode('utf-8'))
+                    if "type" in response_message and response_message["type"] == "Hello message":
+                        RECEIVED_HELLO = True
+                        SHARED_KEY = (response_message["public_key"]**PRIVATE_KEY) % MODULUS
+                        print("Received Hello Message from server")
+                        # print("Shared_key: ", SHARED_KEY)
+                    else:
+                        print("Expected Hello Message, got something else.")
+                except json.JSONDecodeError:
+                    print("Expected Hello Message, got something else.")
+            else:
+                message = decrypt_message(response)
+                if message == "EndSession":
+                    print("Session ended by server. To initiate it again send ClientHello")
+                    RECEIVED_HELLO = False
+                else:
+                    print(f'Received normal message from server: {message}')
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -64,7 +117,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         sys.exit(1)
 
     print(f'Connected to server {HOST}:{TCP_PORT}')
-    
     server_thread = threading.Thread(target=handle_server_messages, args=(s,))
     server_thread.daemon = True
     server_thread.start()
@@ -88,26 +140,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         RECEIVED_HELLO = False
             elif command_id == "2":
                 message = input("Type message content: ")
+                encypted_message = encrypt_message(message)
                 with HELLO_LOCK:
                     if not RECEIVED_HELLO:
                         print("Session is not active, message won't be sent")
                     elif message == "EndSession":
-                        s.sendall(message.encode('utf-8'))
+                        s.sendall(encypted_message)
                         print("Ended session - to initiate it again send Hello Message")
                         RECEIVED_HELLO = False
                     else: 
-                        s.sendall(message.encode('utf-8'))
+                        s.sendall(encypted_message)
                         print("Sent normal message")
             elif command_id == "3":
                 message = "EndSession"
+                encypted_message = encrypt_message(message)
                 with HELLO_LOCK:
-                    s.sendall(message.encode('utf-8'))
-                    print("Ended session - to initiate it again send ClientHello")
-                    RECEIVED_HELLO = False
+                    if RECEIVED_HELLO:
+                        s.sendall(encypted_message)
+                        print("Ended session - to initiate it again send ClientHello")
+                        RECEIVED_HELLO = False
+                    else:
+                        print("Session is not active, message won't be sent")
             else:
                 print("Invalid option. Please try again.")
     except KeyboardInterrupt:
         print("\nClient shutting down.")
-    finally:
         s.close()
-        print("Socket closed. Exiting client.")
--- Projekt/server_python/server.py
+++ Projekt/server_python/server.py
@@ -2,6 +2,10 @@ import socket
 import argparse
 import json
 import threading
+from Crypto.Cipher import AES
+from Crypto.Hash import HMAC, SHA256
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import unpad
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-k", "--private_key", type=int)
@@ -14,18 +18,75 @@ TCP_PORT = 8080
 PRIVATE_KEY = args.private_key
 
 CLIENTS = {} 
-CLIENTS_READY = {}
 CLIENTS_LOCK = threading.Lock()
 
 
-def handle_serving_clients(server_socket: socket.socket):
+def encrypt_message(message, shared_key):
+    expanded_key = SHA256.new(str(shared_key).encode('utf-8')).digest()
+    
+    iv = get_random_bytes(16)
+    cipher = AES.new(expanded_key, AES.MODE_CBC, iv)
+    
+    padding_length = 16 - (len(message) % 16)
+    padded_message = message + chr(padding_length) * padding_length
+    
+    ciphertext = cipher.encrypt(padded_message.encode('utf-8'))
+    
+    hmac = HMAC.new(expanded_key, digestmod=SHA256)
+    hmac.update(iv + ciphertext)
+    
+    to_send_message = iv + ciphertext + hmac.digest()
+    
+    return to_send_message
+
+
+def decrypt_message(encrypted_message, shared_key):
+    print(shared_key)
+    print(encrypted_message)
+    iv = encrypted_message[:16]  
+    ciphertext = encrypted_message[16:-32]  
+    received_hmac = encrypted_message[-32:]  
+    
+
+    expanded_key = SHA256.new(str(shared_key).encode('utf-8')).digest()
+
+    hmac = HMAC.new(expanded_key, digestmod=SHA256)
+
+    hmac.update(iv + ciphertext)
+    print(f"IV: {iv}")
+    print(f"Ciphertext: {ciphertext}")
+    print(f"Received HMAC: {received_hmac}")
+    print(f"Computed HMAC: {hmac.digest()}")
+    try:
+        hmac.verify(received_hmac)
+    except ValueError:
+        raise ValueError("Invalid MAC")
+
+    cipher = AES.new(expanded_key, AES.MODE_CBC, iv)
+    
+
+    decrypted_data = unpad(cipher.decrypt(ciphertext), 16)  
+    return decrypted_data.decode('utf-8')
+
+
+def clients_ready():
+    result = ''
+    with CLIENTS_LOCK:
+        for key in CLIENTS.keys():
+            if CLIENTS[key][1]:
+                result += f'User at address {key} READY for messages\n'
+            else:
+                result += f'User at address {key} NOT READY for messages\n'
+    return result
+
+
+def handle_serving_clients(server_socket):
     while True:
         conn, addr = server_socket.accept()
-        print(f"Client connected: {addr}")
         addr_str = f"{addr[0]}:{addr[1]}"
+        print(f"Client connected: {addr_str}")
         with CLIENTS_LOCK:
-            CLIENTS[addr_str] = conn
-            CLIENTS_READY[addr_str] = False
+            CLIENTS[addr_str] = [conn, False, -1]
         client_thread = threading.Thread(target=handle_client, args=(conn, addr_str, PRIVATE_KEY,))
         client_thread.daemon = True
         client_thread.start()
@@ -34,18 +95,26 @@ def handle_serving_clients(server_socket: socket.socket):
 def handle_client(conn, addr_str, private_key):
     while True:
         data = conn.recv(1024)
+        if not data:
+                print(f'Client {addr_str} has closed the connection.')
+                conn.close()
+                with CLIENTS_LOCK:
+                    del CLIENTS[addr_str]
+                break
         with CLIENTS_LOCK:
-            if not CLIENTS_READY[addr_str]:      
+            if not CLIENTS[addr_str][1]:      
                 try:
                     message = json.loads(data.decode('utf-8'))
                     if "type" in message and message["type"] == "Hello message":
-                        print(f"Received Hello Message from {addr_str}: {message}")  
-                        CLIENTS_READY[addr_str] = True
+                        print(f"Received Hello Message from {addr_str}")  
+                        CLIENTS[addr_str][1] = True
                         public_key = (message["base"] ** private_key) % message["modulus"]
                         response = {
                             "type": "Hello message",
                             "public_key": public_key
                         }
+                        CLIENTS[addr_str][2] = (message["public_key"]**PRIVATE_KEY) % message['modulus']
+                        # print("Shared_key: ", CLIENTS[addr_str][2])
                         conn.sendall(json.dumps(response).encode('utf-8'))
                         print(f"Sent Hello Message response to {addr_str}")
                     else:
@@ -53,43 +122,47 @@ def handle_client(conn, addr_str, private_key):
                 except json.JSONDecodeError:
                     print(f"Received message from {addr_str}, but ignoring until Hello Message.")
             else:
-                message = data.decode('utf-8')
+                message = decrypt_message(data, CLIENTS[addr_str][2])
                 if message == "EndSession":
                     print(f'Session ended by {addr_str}. Can''t send messages there')
-                    CLIENTS_READY[addr_str]= False
+                    CLIENTS[addr_str][1]= False
+                    CLIENTS[addr_str][2]= -1
                 else:
                     print(f'Received normal message from {addr_str}: {message}')
 
-            
 
 def send_message_to_client(client_ip, message):
     with CLIENTS_LOCK:
-        if client_ip in CLIENTS_READY and CLIENTS_READY[client_ip]:
-            conn = CLIENTS[client_ip]
-            conn.sendall(message.encode('utf-8'))
+        if client_ip in CLIENTS and CLIENTS[client_ip][1]:
+            conn = CLIENTS[client_ip][0]
+            encypted_message = encrypt_message(message, CLIENTS[client_ip][2])
+            conn.sendall(encypted_message)
             if message == "EndSession":
                 print(f'Ending connection with {client_ip}')
-                CLIENTS_READY[client_ip] = False
+                CLIENTS[client_ip][1] = False
+                CLIENTS[client_ip][2]= -1
             else:
                 print(f"Sent message to {client_ip}: {message}")
         else:
             print(f"No active connection to {client_ip}")
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind((IP, TCP_PORT))
-    server_socket.listen()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((IP, TCP_PORT))
+    s.listen()
     print(f"Server listening on {IP}:{TCP_PORT}")
 
-    serving_clients_thread = threading.Thread(target=handle_serving_clients, args=(server_socket,))
+    serving_clients_thread = threading.Thread(target=handle_serving_clients, args=(s,))
     serving_clients_thread.daemon = True
     serving_clients_thread.start()
-
-    while True:
-        print("Clients ready for messages: ", CLIENTS_READY)
-        client_ip = input("Enter client IP to send message to:\n")
-        message = input("Enter message to send:\n")
-
-        send_message_to_client(client_ip, message)
+    try:
+        while True:
+            print(clients_ready())
+            client_ip = input("Enter client IP to send message to:\n")
+            message = input("Enter message to send:\n")
+            send_message_to_client(client_ip, message)
+    except KeyboardInterrupt:
+        s.close()
+        
 
 
